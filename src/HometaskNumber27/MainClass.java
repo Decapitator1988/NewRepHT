9140efcfb7af2e93f08135010a146a7f5285ca0b
Decapitator88 <decapitator1988@gmail.com>
2017-11-10 11:30:37 +0300

    27th done

--- src/HometaskNumber27/MainClass.java
+++ src/HometaskNumber27/MainClass.java
@@ -22,37 +22,33 @@ public class MainClass {
                 new Person("Andre Pirlo"),
                 new Person("Oliver Bierhovv"),
                 new Person("Alvaro Recoba"),
-                new Person("Ryan Giggs")
-                );
-        showMiddleAgePerson(list, (p) -> p.getAge()>25&&p.getAge()<45);
-        Stream<Person> personStream =  list.stream();
-        System.out.println(personStream.count());
-        showPersonStartsNameWithA(personStream);
+                new Person("Stephen Gerard"),
+                new Person("Clarense Seedorf"),
+                new Person("Tony Kroos"),
+                new Person("Tomas Muller"),
+                new Person("Ashley Cole")
+        );
+        System.out.println("Persons with age over 25 and under 45: ");
+        showMiddleAgePerson(list, (p) -> p.getAge() > 25 && p.getAge() < 45);
+        Stream<Person> personStream = list.stream();
 
-//
-//        Person person1 = new Person("Cristiano Romnaldo");
-//        Person person2 = new Person("Andreas Iniesta");
-//        Person person3 = new Person("Pablo Esconar");
-//        Person person4 = new Person("Andre Pirlo");
-//        Person person5 = new Person("Rivaldo");
-//        Person person6 = new Person("Oliver Bierhovv");
-//        Person person7 = new Person("Oliver Kahn");
-//        Person person8 = new Person("David Backham");
-//        Person person9 = new Person("Ryan Giggs");
-
+        System.out.println("Total number of persons: " + personStream.count());
+        System.out.println();
+        System.out.println("Persons, which name starts with letter A: ");
+        showPersonStartsNameWithA(personStream, list);
     }
-    public static void showMiddleAgePerson(List<Person> list, Predicate<Person> predicate){
-        for (Person p: list ) {
-            if(predicate.test(p)){
+
+    private static void showMiddleAgePerson(List<Person> list, Predicate<Person> predicate) {
+        for (Person p : list) {
+            if (predicate.test(p)) {
                 System.out.println(p + " ");
             }
         }
         System.out.println();
     }
 
-    public static void showPersonStartsNameWithA(Stream<Person> streamm){
-
-        streamm.filter(person -> person.getName().startsWith("A"))
-                .forEach(person ->  );
+    private static void showPersonStartsNameWithA(Stream<Person> stream, Collection<Person> collection) {
+        collection.stream().filter(person -> person.getName().startsWith("A"))
+                .forEach(System.out::println);
     }
 }
